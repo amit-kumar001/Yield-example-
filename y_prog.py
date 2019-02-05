02970434de0a0c0e22ba963c97ab123faa6013f2
amit-kumar001 <47202519+amit-kumar001@users.noreply.github.com>
2019-02-05 18:10:21 +0530

    Update y_prog.py

--- y_prog.py
+++ y_prog.py
@@ -1,11 +1,11 @@
 # First we define next() function 
 def next():
-    i=1           # declare the vale of i=1
+    i=1           # declare the value of i=1
     while True:   # while True is for infinte loop 
         yield i*i #  yield use for iteration over the multiple vales 
         i+=1      # for increment in term of 1
 
-for result in next(): # here result is iterate through values of function next()
+for result in next(): # here we are iterating over the function next()
     if result > 100: # now we set the condition to stop while loop
         break        # break  statement  use to stop while loop
     print(result)
